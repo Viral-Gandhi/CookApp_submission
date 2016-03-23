94268ce6a87564dbcd26932114873337f3b1b617
Viral-Gandhi <zcesvga@ucl.ac.uk>
2016-03-23 16:30:39 +0000

    Updated 23/03/16 @1630 (working searched Array)

--- app/src/main/java/com/example/viral/cookapp_recipeselect/RecipeSelect.java
+++ app/src/main/java/com/example/viral/cookapp_recipeselect/RecipeSelect.java
@@ -127,24 +127,4 @@ public class RecipeSelect extends AppCompatActivity {
         );
         AppIndex.AppIndexApi.start(client, viewAction);
     }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "RecipeSelect Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app deep link URI is correct.
-                Uri.parse("android-app://com.example.viral.cookapp_recipeselect/http/host/path")
-        );
-        AppIndex.AppIndexApi.end(client, viewAction);
-        client.disconnect();
-    }
 }
--- app/src/main/java/com/example/viral/cookapp_recipeselect/SearchRecipes.java
+++ app/src/main/java/com/example/viral/cookapp_recipeselect/SearchRecipes.java
@@ -1,10 +1,14 @@
 package com.example.viral.cookapp_recipeselect;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SearchView;
 
 public class SearchRecipes extends AppCompatActivity {
 
@@ -14,17 +18,48 @@ public class SearchRecipes extends AppCompatActivity {
         setContentView(R.layout.activity_search_recipes);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        populatingListRecs();
-    }
-    private void populatingListRecs() {
-        final int NO_RECIPES = 3;
-        String[] AllRecipes = new String[NO_RECIPES];
+
+        String[] AllRecipes = new String[3];
         AllRecipes[0] = "Falafel";
         AllRecipes[1] = "Korma";
         AllRecipes[2] = "Chicken Tikka";
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(SearchRecipes.this, R.layout.activity_search_recipes, AllRecipes);
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.listviewrecsearchlayout, AllRecipes);
         ListView listRecs = (ListView) findViewById(R.id.listViewSearchRec);
         listRecs.setAdapter(adapter);
+        listRecs.setOnItemClickListener(new openIntents());
+
+        SearchView SearchRec = (SearchView) findViewById(R.id.searchViewSearchRec);
+        SearchRec.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+
+            @Override
+            public boolean onQueryTextSubmit(String RecSearched) {
+            return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String RecSearched) {
+
+                    adapter.getFilter().filter(RecSearched);
+
+                return false;
+            }
+        });
     }
-  }
+
+    public class openIntents implements AdapterView.OnItemClickListener{
+        public void onItemClick(AdapterView<?> parent, View viewClicked, int position, long id) {
+            if(position==0){
+                Intent openFalafel = new Intent(SearchRecipes.this, RecipeOne.class);
+                startActivity(openFalafel);
+            }
+            else if (position == 1) {
+                Intent openKorma = new Intent(SearchRecipes.this, CuisineFind.class);
+                startActivity(openKorma);
+            }
+        }
+    }
+
+
+
+}
